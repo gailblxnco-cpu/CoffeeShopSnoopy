7951598ef9a0e9aa8ce2cc270aeb2f41ad633e0f
Gail Blanco <gail.blxnco@gmail.com>
2025-11-14 20:01:46 -0600

    Commit modificado del proyecto >.<

--- src/main/java/cafeteria/cafeteria/HelloApplication.java
+++ src/main/java/cafeteria/cafeteria/HelloApplication.java
@@ -3,6 +3,7 @@ package cafeteria.cafeteria;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -10,6 +11,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+
+        Image icon = new Image(getClass().getResourceAsStream("images/snoopy.png"));
+        stage.getIcons().add(icon);
+
         stage.setTitle("Café Snoopy");
         stage.setScene(scene);
         stage.setResizable(false);
